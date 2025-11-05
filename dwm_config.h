4655279571cb4140ef43445e35c4e1ed5e8a39ac
Petru Stoica <74964819+petrustoica@users.noreply.github.com>
2025-11-05 18:16:35 +0200

    Update dwm_config.h

--- dwm_config.h
+++ dwm_config.h
@@ -6,13 +6,13 @@ static const unsigned int borderpx  = 0;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 0;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Fira Code:pixelsize=20:bold" };
-static const char dmenufont[]       = "Fira Code:pixelsize=20:bold";
+static const char *fonts[]          = { "BlexMono Nerd Font:bold:pixelsize=24" };
+static const char dmenufont[]       = "BlexMono Nerd Font:bold:pixelsize=24";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_green[]      = "#008000";
+static const char col_selected[]      = "#004512"; //orange = bba000
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
@@ -20,7 +20,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "", "", "", "", "" };
+static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9"};
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -28,21 +28,21 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "firefox",  NULL,       NULL,       1 << 0,       0,           -1 }, 
-    { "Steam",    NULL,       NULL,       1 << 4,       0,           -1 },
-    { "discord",  NULL,       NULL,       1 << 1,       0,           -1 },
+	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
+	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
 };
 
 /* layout(s) */
-static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
+static const float mfact     = 0.5; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
 static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
+static const int refreshrate = 240;  /* refresh rate (per second) for client move/resize */
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-    { "|M|",      centeredmaster },
-	{ "[]=",      tile }    /* first entry is default */
+	//{ "[]=",      tile },    /* first entry is default */
+	{ "",      tile },    /* first entry is default */
 };
 
 /* key definitions */
@@ -59,14 +59,13 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_green, "-sf", col_gray4, NULL };
-static const char *bluetoothcmd[] = { "dmenu-bluetooth", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_green, "-sf", col_gray4, NULL };
-static const char *networkmanagercmd[] = { "networkmanager_dmenu", NULL };
-static const char *qalccmd[] = { "dmenu_run", "-C", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_green, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_selected, "-sf", col_gray4, NULL };
+static const char *networkmanager_dmenu[] = { "networkmanager_dmenu", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_selected, "-sf", col_gray4, NULL };
+static const char *bluetoothcmd[] = { "dmenu-bluetooth", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_selected, "-sf", col_gray4, NULL };
+static const char *qalccmd[] = { "dmenu_run", "-C", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_selected, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
-static const char *savereplaycmd[]  = { "save_replay", NULL };
 static const char *flameshotcmd[] = {"flameshot", "gui", NULL};
-static const char *rofimojicmd[] = {"rofimoji", "--skin-tone", "neutral", NULL};
+static const char *rofimojicmd[] = {"rofimoji", "-r", "", "--skin-tone", "neutral", NULL};
 
 static const char *upvol[] = {"volume", "-a", "0.025", NULL};
 static const char *downvol[] = {"volume", "-a", "-0.025", NULL};
@@ -95,10 +94,9 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_period, spawn,          {.v = rofimojicmd } },
 	{ MODKEY,                       XK_r,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_b,      spawn,          {.v = bluetoothcmd } },
-	{ MODKEY,                       XK_n,      spawn,          {.v = networkmanagercmd } },
+    { MODKEY,                       XK_n,      spawn,          {.v = networkmanager_dmenu } },
 	{ MODKEY,                       XK_f,      spawn,          {.v = qalccmd } },
 	{ MODKEY,                       XK_q,      spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_t,      spawn,          {.v = savereplaycmd } },
 	{ MODKEY,                       XK_z,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
@@ -116,7 +114,7 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
 	//{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
-	//{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
 	{ 0,                            XK_Print,  spawn,          {.v = flameshotcmd}},
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
@@ -145,4 +143,3 @@ static const Button buttons[] = {
 	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
 	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
 };
-
